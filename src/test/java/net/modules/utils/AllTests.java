1546427d920e44de588d0c67f099bb8aeaff5c77
nhr407 <nhr407@163.com>
2016-06-05 23:44:17 +0800

    restart

---
+++ src/test/java/net/modules/utils/AllTests.java
@@ -0,0 +1,11 @@
+package net.modules.utils;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ AddSubTest.class, DivTest.class })
+public class AllTests {
+
+}
