e13741266ba7004c79ebda938f02342fb060b2a8
niuhr <nhr407@163.com>
2016-06-27 13:37:35 +0800

    antiSamy

---
+++ src/main/java/net/modules/tags/SafeHtmlTag.java
@@ -0,0 +1,77 @@
+package net.modules.tags;
+
+import java.io.FileNotFoundException;
+import java.net.URL;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.tagext.TagSupport;
+
+import org.apache.log4j.Logger;
+import org.owasp.validator.html.AntiSamy;
+import org.owasp.validator.html.CleanResults;
+import org.owasp.validator.html.Policy;
+
+/**
+ * JSP tag that renders AntiSamy-filtered HTML, useful for displaying HTML markup in a safe way.
+ */
+public class SafeHtmlTag extends TagSupport {
+	private static final long serialVersionUID = 1L;
+    private static final Logger LOG = Logger.getLogger(SafeHtmlTag.class);
+    private static final AntiSamy DEFAULT_ANTI_SAMY = new AntiSamy(loadPolicy("antisamy-default.xml"));
+    private static final Map<String, Policy> POLICY_CACHE = new ConcurrentHashMap<String, Policy>();
+
+    private String text;
+    private String policyFile;
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public void setPolicyFile(String policyFile) {
+        this.policyFile = policyFile;
+    }
+
+    public int doStartTag() throws JspException {
+        try {
+            CleanResults scan = getAntiSamy().scan(text);
+            if (scan.getNumberOfErrors() > 0) {
+                LOG.warn("antisamy encountered problem with input: " + scan.getErrorMessages());
+            }
+            pageContext.getOut().print(scan.getCleanHTML());
+        } catch (Exception e) {
+            throw new IllegalStateException(e.getMessage(), e);
+        }
+        return SKIP_BODY;
+    }
+
+    public int doEndTag() throws JspException {
+        return EVAL_PAGE;
+    }
+
+    protected AntiSamy getAntiSamy() {
+        if (policyFile == null || "".equals(policyFile)) {
+            return DEFAULT_ANTI_SAMY;
+        }
+        return new AntiSamy(loadPolicy(policyFile));
+    }
+
+    private static Policy loadPolicy(String name) {
+        if (POLICY_CACHE.containsKey(name)) {
+            return POLICY_CACHE.get(name);
+        }
+        try {
+            LOG.debug("Loading policy file '" + name + "' from classpath");
+            URL url = SafeHtmlTag.class.getClassLoader().getResource(name);
+            if (url == null || url.getFile() == null) {
+                throw new FileNotFoundException("classpath file '" + name + "' was not found!");
+            }
+            Policy loadedPolicy = Policy.getInstance(url.getFile());
+            POLICY_CACHE.put(name, loadedPolicy);
+            return loadedPolicy;
+        } catch (Exception e) {
+            throw new IllegalStateException(e.getMessage(), e);
+        }
+    }
+}
---
+++ src/test/java/net/modules/filter/xssfilter/AntiSamyFilterTest.java
@@ -0,0 +1,83 @@
+package net.modules.filter.xssfilter;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.junit.Assert.*;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletRequestWrapper;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.mock.web.MockFilterChain;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+/**
+ * Unit test for AntiSamyFilterTest
+ */
+public class AntiSamyFilterTest {
+	private AntiSamyFilter filter;
+
+    //mocks
+    private MockHttpServletRequest request;
+    private MockHttpServletResponse response;
+    private MockFilterChain filterChain;
+
+    @Before
+    public void setUp() throws Exception {
+        filter = new AntiSamyFilter();
+        filter.init(null);
+
+        request = new MockHttpServletRequest();
+        response = new MockHttpServletResponse();
+        filterChain = new MockFilterChain();
+    }
+
+    @Test
+    public void testValidParamPassesThrough() throws Exception {
+        String param = "valid_param";
+        String value = "valid_value";
+        request.setParameter(param, value);
+        filter.doFilter(request, response, filterChain);
+
+        ServletRequest wrappedRequest = filterChain.getRequest();
+        assertEquals(wrappedRequest.getParameter(param), value);
+    }
+
+    @Test
+    public void testInvalidParamIsFiltered() throws Exception {
+        String param = "valid_param";
+        request.setParameter(param, "<SCRIPT SRC=//ha.ckers.org/.j>");
+        filter.doFilter(request, response, filterChain);
+
+        ServletRequest wrappedRequest = filterChain.getRequest();
+        assertThat(wrappedRequest.getParameter(param), not(containsString("SCRIPT")));
+    }
+
+    @Test
+    public void testValidContentNearInvalidContentSurvives() throws Exception {
+        String param = "valid_param";
+        request.setParameter(param, "<SCRIPT></SCRIPT> should pass through");
+        filter.doFilter(request, response, filterChain);
+
+        ServletRequest wrappedRequest = filterChain.getRequest();
+        assertThat(wrappedRequest.getParameter(param), containsString("pass through"));
+        assertThat(wrappedRequest.getParameter(param), not(containsString("SCRIPT")));
+    }
+
+    @Test
+    public void testNonHttpRequestPassesThrough() throws Exception {
+        String param = "valid_param";
+        String value = "should pass through";
+        ServletRequest servletRequest = new ServletRequestWrapper(request);
+        request.setParameter(param, value);
+        filter.doFilter(servletRequest, response, filterChain);
+
+        ServletRequest originalRequest = filterChain.getRequest();
+        assertThat(originalRequest.getParameter(param), equalTo(value));
+        assertThat(originalRequest, is(originalRequest));
+    }
+}
